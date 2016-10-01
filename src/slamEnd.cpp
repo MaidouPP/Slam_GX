f4c21daeec1f52ddbcc92f409533e5a25917ecab
Shixin Li <leept416@gmail.com>
2016-10-01 21:34:49 +0800

    g2o

--- src/slamEnd.cpp
+++ src/slamEnd.cpp
@@ -1,11 +1,18 @@
+/*************************************************************************
+	> File Name: rgbd-slam-tutorial-gx/part V/src/visualOdometry.cpp
+	> Author: xiang gao
+	> Mail: gaoxiang12@mails.tsinghua.edu.cn
+	> Created Time: 2015年08月15日 星期六 15时35分42秒
+    * add g2o slam end to visual odometry
+ ************************************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 using namespace std;
 
 #include "slamBase.h"
-
-#include <g2o/types/slam3d/types_slam3d.h>  // types of vertex
+#include <g2o/types/slam3d/types_slam3d.h>
 #include <g2o/core/sparse_optimizer.h>
 #include <g2o/core/block_solver.h>
 #include <g2o/core/factory.h>
@@ -16,151 +23,156 @@ using namespace std;
 #include <g2o/core/robust_kernel_factory.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 
-// give the index and return this frame
-FRAME readFrame( int index, ParameterReader& pd);
-// measure the motion
-double normofTransform( cv::Mat rvec, cv::Mat tvec);
 
-int main( int argc, char** argv) {
-  ParameterReader pd;
-  int startIndex = atoi(pd.getData("start_index").c_str());
-  int endIndex = atoi(pd.getData("end_index").c_str());
+// 给定index，读取一帧数据
+FRAME readFrame( int index, ParameterReader& pd );
+// 估计一个运动的大小
+double normofTransform( cv::Mat rvec, cv::Mat tvec );
 
-  // initialize
-  cout << "Initializing... " << endl;
-  int currIndex = startIndex;
-  FRAME lastFrame = readFrame( currIndex, pd); // last frame
-  // we always compare this one and last frame
-  string detector = pd.getData("detector");
-  string descriptor = pd.getData("descriptor");
-  CAMERA_INTRINSIC_PARAMETERS camera = getDefaultCamera();
-  computeKeyPointsAndDesp( lastFrame, detector, descriptor );
-  PointCloud::Ptr cloud = image2PointCloud( lastFrame.rgb, lastFrame.depth, camera );
+int main( int argc, char** argv )
+{
+    // 前面部分和vo是一样的
+    ParameterReader pd;
+    int startIndex  =   atoi( pd.getData( "start_index" ).c_str() );
+    int endIndex    =   atoi( pd.getData( "end_index"   ).c_str() );
 
-  pcl::visualization::CloudViewer viewer("viewer");
-
-  bool visualize = pd.getData("visualize_pointcloud")==string("yes");
-  int min_inliers = atoi( pd.getData("min_inliers").c_str() );
-  double max_norm = atof( pd.getData("max_norm").c_str() );
-
-  /*************
-  // add optimization here
-  ************/
-
-  // choose a optimization method
-  typedef g2o::BlockSolver_6_3 SlamBlockSolver;
-  typedef g2o::LinearSolverCSparse< SlamBlockSolver::PoseMatrixType > SlamLinearSolver;
-
-  // initilize solver
-  SlamLinearSolver* linearSolver = new SlamLinearSolver();
-  linearSolver->setBlockOrdering(false);
-  SlamBlockSolver* blockSolver = new SlamBlockSolver( linearSolver );
-  g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(blockSolver);
-
-  g2o::SparseOptimizer globalOptimizer;  // use it in the end
-  globalOptimizer.setAlgorithm( solver );
-  // no debugging messages
-  globalOptimizer.setVerbose( false );
-
-  // add the first vertec to the graph
-  g2o::VertexSE3* v = new g2o::VertexSE3();
-  v->setId( currIndex );
-  v->setEstimate( Eigen::Isometry3d::Identity() );
-  v->setFixed( true ); // fix the first one. It doesn't need to be optimized.
-  globalOptimizer.addVertex( v );
-
-  int lastIndex = currIndex; 
-
-  for ( currIndex=startIndex+1; currIndex<endIndex; currIndex++) {
-
-    cout << "Reading files..."<< endl;
-    FRAME currFrame = readFrame( currIndex, pd);
-    computeKeyPointsAndDesp( currFrame, detector, descriptor);
-    // compare currFrame and lastFrame
-    RESULT_OF_PNP result = estimateMotion( lastFrame, currFrame, camera);
-    if ( result.inliers < min_inliers)
-      continue;  // not enough inliers, abandon this one
-    // judge if range of motion is too large which is unreasonable
-    double norm = normofTransform(result.rvec, result.tvec);
-    cout << "not = " << norm << endl;
-    if ( norm >= max_norm)
-      continue;
-    Eigen::Isometry3d T  = cvMat2Eigen( result.rvec, result.tvec);
-    cout << "T = " << T.matrix() << endl;
-
-
-    //cloud = joinPointCloud( cloud, currFrame, T, camera );
-
-    // add this vertec to g2o as well as the edge to last frame
-    // vertex part (only need index)
-    g2o::VertexSE3 *v = new g2o::VertexSE3();
-    v->setId( currIndex );
-    v->setEstimate( Eigen::Isometry3d::Identity());
-    globalOptimizer.addVertex(v);
-
-    // edge part
-    g2o::EdgeSE3* edge = new g2o::EdgeSE3();
-    // two index connecting this edge
-    edge->vertices() [0] = globalOptimizer.vertex( lastIndex );
-    edge->vertices() [1] = globalOptimizer.vertex( currIndex );
-
-    // information matrix
-    Eigen::Matrix<double, 6, 6> information = Eigen::Matrix< double, 6,6 >::Identity();
-    // covariance is 0.01, information matrix is 100
-    information(0,0) = information(1,1) = information(2,2) = 100;
-    information(3,3) = information(4,4) = information(5,5) = 100;
-
-    edge->setInformation( information );
-
-    // edge's estimation is the result of PnP
-    edge->setMeasurement( T );
-    // add edge to graph
-    globalOptimizer.addEdge(edge);
+    // initialize
+    cout<<"Initializing ..."<<endl;
+    int currIndex = startIndex; // 当前索引为currIndex
+    FRAME lastFrame = readFrame( currIndex, pd ); // 上一帧数据
+    // 我们总是在比较currFrame和lastFrame
+    string detector = pd.getData( "detector" );
+    string descriptor = pd.getData( "descriptor" );
+    CAMERA_INTRINSIC_PARAMETERS camera = getDefaultCamera();
+    computeKeyPointsAndDesp( lastFrame, detector, descriptor );
+    PointCloud::Ptr cloud = image2PointCloud( lastFrame.rgb, lastFrame.depth, camera );
     
-    //    if ( visualize == true)
-    //      viewer.showCloud( cloud);
+    pcl::visualization::CloudViewer viewer("viewer");
 
-    lastFrame = currFrame;
-    lastIndex = currIndex;
-  }
-  // pcl::io::savePCDFile( "data/result.pcd", *cloud);
+    // 是否显示点云
+    bool visualize = pd.getData("visualize_pointcloud")==string("yes");
 
-  // optimize all of the edges
-  cout<<"optimizing pose graph, vertices: "<<globalOptimizer.vertices().size()<<endl;
-  globalOptimizer.save("./data/result_before.g2o");
-  globalOptimizer.initializeOptimization();
-  globalOptimizer.optimize( 100 ); //可以指定优化步数
-  globalOptimizer.save( "./data/result_after.g2o" );
-  cout<<"Optimization done."<<endl;
+    int min_inliers = atoi( pd.getData("min_inliers").c_str() );
+    double max_norm = atof( pd.getData("max_norm").c_str() );
+    
+    /******************************* 
+    // 新增:有关g2o的初始化
+    *******************************/
+    // 选择优化方法
+    typedef g2o::BlockSolver_6_3 SlamBlockSolver; 
+    typedef g2o::LinearSolverCSparse< SlamBlockSolver::PoseMatrixType > SlamLinearSolver; 
 
-  globalOptimizer.clear();
-  
-  return 0;
+    // 初始化求解器
+    SlamLinearSolver* linearSolver = new SlamLinearSolver();
+    linearSolver->setBlockOrdering( false );
+    SlamBlockSolver* blockSolver = new SlamBlockSolver( linearSolver );
+    g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg( blockSolver );
+
+    g2o::SparseOptimizer globalOptimizer;  // 最后用的就是这个东东
+    globalOptimizer.setAlgorithm( solver ); 
+    // 不要输出调试信息
+    globalOptimizer.setVerbose( false );
+
+    // 向globalOptimizer增加第一个顶点
+    g2o::VertexSE3* v = new g2o::VertexSE3();
+    v->setId( currIndex );
+    v->setEstimate( Eigen::Isometry3d::Identity() ); //估计为单位矩阵
+    v->setFixed( true ); //第一个顶点固定，不用优化
+    globalOptimizer.addVertex( v );
+
+    int lastIndex = currIndex; // 上一帧的id
+
+    for ( currIndex=startIndex+1; currIndex<endIndex; currIndex++ )
+    {
+        cout<<"Reading files "<<currIndex<<endl;
+        FRAME currFrame = readFrame( currIndex,pd ); // 读取currFrame
+        computeKeyPointsAndDesp( currFrame, detector, descriptor );
+        // 比较currFrame 和 lastFrame
+        RESULT_OF_PNP result = estimateMotion( lastFrame, currFrame, camera );
+        if ( result.inliers < min_inliers ) //inliers不够，放弃该帧
+            continue;
+        // 计算运动范围是否太大
+        double norm = normofTransform(result.rvec, result.tvec);
+        cout<<"norm = "<<norm<<endl;
+        if ( norm >= max_norm )
+            continue;
+        Eigen::Isometry3d T = cvMat2Eigen( result.rvec, result.tvec );
+        cout<<"T="<<T.matrix()<<endl;
+        
+        // cloud = joinPointCloud( cloud, currFrame, T, camera );
+
+        // 向g2o中增加这个顶点与上一帧联系的边
+        // 顶点部分
+        // 顶点只需设定id即可
+        g2o::VertexSE3 *v = new g2o::VertexSE3();
+        v->setId( currIndex );
+        v->setEstimate( Eigen::Isometry3d::Identity() );
+        globalOptimizer.addVertex(v);
+        // 边部分
+        g2o::EdgeSE3* edge = new g2o::EdgeSE3();
+        // 连接此边的两个顶点id
+        edge->vertices() [0] = globalOptimizer.vertex( lastIndex );
+        edge->vertices() [1] = globalOptimizer.vertex( currIndex );
+        // 信息矩阵
+        Eigen::Matrix<double, 6, 6> information = Eigen::Matrix< double, 6,6 >::Identity();
+        // 信息矩阵是协方差矩阵的逆，表示我们对边的精度的预先估计
+        // 因为pose为6D的，信息矩阵是6*6的阵，假设位置和角度的估计精度均为0.1且互相独立
+        // 那么协方差则为对角为0.01的矩阵，信息阵则为100的矩阵
+        information(0,0) = information(1,1) = information(2,2) = 100;
+        information(3,3) = information(4,4) = information(5,5) = 100;
+        // 也可以将角度设大一些，表示对角度的估计更加准确
+        edge->setInformation( information );
+        // 边的估计即是pnp求解之结果
+        edge->setMeasurement( T );
+        // 将此边加入图中
+        globalOptimizer.addEdge(edge);
+
+        lastFrame = currFrame;
+        lastIndex = currIndex;
+
+    }
+
+    // pcl::io::savePCDFile( "data/result.pcd", *cloud );
+    
+    // 优化所有边
+    cout<<"optimizing pose graph, vertices: "<<globalOptimizer.vertices().size()<<endl;
+    globalOptimizer.save("./data/result_before.g2o");
+    globalOptimizer.initializeOptimization();
+    globalOptimizer.optimize( 100 ); //可以指定优化步数
+    globalOptimizer.save( "./data/result_after.g2o" );
+    cout<<"Optimization done."<<endl;
+
+    globalOptimizer.clear();
+
+    return 0;
 }
 
-FRAME readFrame( int index, ParameterReader& pd) {
-  FRAME f;
-  string rgbDir   =   pd.getData("rgb_dir");
-  string depthDir =   pd.getData("depth_dir");
+FRAME readFrame( int index, ParameterReader& pd )
+{
+    FRAME f;
+    string rgbDir   =   pd.getData("rgb_dir");
+    string depthDir =   pd.getData("depth_dir");
+    
+    string rgbExt   =   pd.getData("rgb_extension");
+    string depthExt =   pd.getData("depth_extension");
 
-  string rgbExt   =   pd.getData("rgb_extension");
-  string depthExt =   pd.getData("depth_extension");
+    stringstream ss;
+    ss<<rgbDir<<index<<rgbExt;
+    string filename;
+    ss>>filename;
+    f.rgb = cv::imread( filename );
 
-  stringstream ss;
-  ss<<rgbDir<<index<<rgbExt;
-  string filename;
-  ss >> filename;
-  f.rgb = cv::imread( filename );
+    ss.clear();
+    filename.clear();
+    ss<<depthDir<<index<<depthExt;
+    ss>>filename;
 
-  ss.clear();
-  filename.clear();
-  ss<<depthDir<<index<<depthExt;
-  ss>>filename;
-
-  f.depth = cv::imread( filename, -1 );
-  return f;
+    f.depth = cv::imread( filename, -1 );
+    f.frameID = index;
+    return f;
 }
 
-double normofTransform( cv::Mat rvec, cv::Mat tvec ) {
-  return fabs(min(cv::norm(rvec), 2*M_PI-cv::norm(rvec)))+ fabs(cv::norm(tvec));
+double normofTransform( cv::Mat rvec, cv::Mat tvec )
+{
+    return fabs(min(cv::norm(rvec), 2*M_PI-cv::norm(rvec)))+ fabs(cv::norm(tvec));
 }
